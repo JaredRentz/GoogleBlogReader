71115d7ac73e1d7c68da155c6f191cfb567c5fc5
Jr6Over6 <Jared@Jared.att.net>
2015-12-31 16:22:47 -0500

    first commit

---
+++ DataSource.swift
@@ -0,0 +1,11 @@
+//
+//  Constant.swift
+//  Blogg Reader
+//
+//  Created by Jared Rentz on 12/31/15.
+//  Copyright © 2015 UXOThings LLC. All rights reserved.
+//
+
+import Foundation
+
+let BASE_URL = 
